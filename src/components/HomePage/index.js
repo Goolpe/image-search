5e98661ce3a6576d09b5985faf5aea5e464ba167
Artur Khabirov <gooolpe@gmail.com>
2018-10-02 14:23:47 +0500

    filters date and licenses

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
 import ImageBlock from '../ImageBlock';
+import ArrowDropUp from '@material-ui/icons/ArrowDropUp';
+import ArrowDropDown from '@material-ui/icons/ArrowDropDown';
+import Sort from '@material-ui/icons/Sort';
+
+const API = 'https://api.flickr.com/services/rest/?method=';
+const API_KEY = '&api_key=501cd1f79f31f6d00da6faed96ee96ae&format=json&nojsoncallback=1';
 
 class HomePage extends Component {
 	constructor(props){
 		super(props);
 		this.state={
 			inputText: '',
-			input: 'dogs'
+			input: 'dogs',
+			sortByDateUp: false,
+			sortByDateDown: true,
+			licenses: [],
+			licenseFilter: ""
 		}
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleClick = this.handleClick.bind(this);
 	}
 
+	componentDidMount(){
+		fetch(API + "flickr.photos.licenses.getInfo" + API_KEY)
+			.then(response => response.json() )
+			.then(data => {
+				this.setState({
+					licenses: data.licenses.license
+				})
+			})
+	}
 //handling change of value input
 	handleChange(e){
 		let value = e.target.value
@@ -26,9 +46,19 @@ class HomePage extends Component {
 		this.setState({
 			input: this.state.inputText.toLowerCase()
 		})
+	}
 
+//handling click on filter
+	handleClick(){
+		this.setState({
+			sortByDateUp: !this.state.sortByDateUp,
+			sortByDateDown: !this.state.sortByDateDown
+		})
 	}
   render() {
+  	const LICENSES_LIST = this.state.licenses.map((license,index) => 
+  		<option key={index} value={license.id}>{license.name}</option>
+  	)
     return (
       <div className="container">
       	<h1>Search photos</h1>
@@ -36,7 +66,13 @@ class HomePage extends Component {
 	      	<input value={this.state.inputText} id="inputText" onChange={this.handleChange} placeholder="Dogs" required/>
 	      	<button type="submit" >Search</button>
       	</form>
-      	<ImageBlock method="flickr.photos.search" nid={"&text=" + this.state.input}/>
+      	<div className="filter_container">
+      	<button onClick={this.handleClick} className="filter_icons">{this.state.sortByDateDown ? <ArrowDropUp/> : <ArrowDropDown/>}Date</button>
+      	<select value={this.state.licenseFilter} id="licenseFilter" onChange={this.handleChange}>
+			    {LICENSES_LIST}
+			  </select>
+      	</div>
+      	<ImageBlock method="flickr.photos.search" nid={"&text=" + this.state.input} sortByDateUp={this.state.sortByDateUp} sortByDateDown={this.state.sortByDateDown} license={this.state.licenseFilter}/>
       </div>
     );
   }
--- src/components/ImageBlock.js
+++ src/components/ImageBlock.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 import {Link} from 'react-router-dom';
 
 const API = 'https://api.flickr.com/services/rest/?method=';
-const API_KEY = '&api_key=4e223febd1051d6edac11a8ce2f42d36&format=json&nojsoncallback=1';
+const API_KEY = '&api_key=501cd1f79f31f6d00da6faed96ee96ae&format=json&nojsoncallback=1';
 
 class ImageBlock extends Component {
 	constructor(props){
@@ -20,9 +20,8 @@ class ImageBlock extends Component {
 
 	componentDidMount(){
 //add event scroll
-		window.addEventListener('scroll', this.handleScroll)
-		this.fetchData()
-
+		window.addEventListener('scroll', this.handleScroll);
+		this.fetchData();
 	}
 
 	fetchData(){
@@ -36,7 +35,6 @@ class ImageBlock extends Component {
 		fetch(API + this.props.method + API_KEY + this.props.nid)
 			.then(response => response.json() )
 			.then(data => {
-
 // getting photos info
 				data.photos.photo.map(item=>{
 					return fetch(API + 'flickr.photos.getInfo' + API_KEY + '&photo_id=' + item.id + '&secret=' + item.secret)
@@ -51,6 +49,7 @@ class ImageBlock extends Component {
 				})
 			})
 	}
+
 	componentDidUpdate(nextProps){
 		if (this.props.nid !== nextProps.nid){
 			this.fetchData()
@@ -66,23 +65,40 @@ class ImageBlock extends Component {
 
 	loadMoreItems(){
 //show loading indicator
-			this.setState({
-				loading: true
-			})
+		this.setState({
+			loading: true
+		})
 
-//timeout before load data
-			setTimeout(()=>{
-				 this.setState({
-				 	loading: false,
-	        		items: this.state.items + 10,
-	      });
-			}, 2500)
+	//timeout before load data
+		setTimeout(()=>{
+			 this.setState({
+			 	loading: false,
+	    		items: this.state.items + 10,
+	  });
+		}, 2500)
 	}
 
   render() {
+  	var photosWithFilter;
 
+// sort by date down
+
+  	this.props.sortByDateDown && (photosWithFilter = this.state.photos.sort((a,b) =>{
+  		return new Date(b.photo.dates.taken) - new Date(a.photo.dates.taken)
+  	}))
+
+// sort by date up
+
+  	this.props.sortByDateUp && (photosWithFilter = this.state.photos.sort((a,b) =>{
+  		return new Date(a.photo.dates.taken) - new Date(b.photo.dates.taken)
+  	}))
+
+// licenses filter
+
+  	this.props.license && (photosWithFilter = photosWithFilter.filter(item => this.props.license === item.photo.license))
+  	console.log(photosWithFilter)
 // creating cards
-  	const photosList = this.state.photos.map((item,index)=> 
+  	const PHOTOS_LIST = photosWithFilter.map((item,index)=> 
         	<div className="card" key={index}>
 	        		<img className="card_image" alt={item.photo.title} src={'https://farm' + item.photo.farm + '.staticflickr.com/' + item.photo.server + '/' + item.photo.id + '_' + item.photo.secret + '.jpg'} />
 	        		<div className="card_info">
@@ -97,7 +113,7 @@ class ImageBlock extends Component {
     return (
       <div className="container">
 		<div className="cards_container">
-      		{photosList}
+      		{PHOTOS_LIST}
       	</div>
       	<div className="loading">
       		{this.state.loading && <h1>loading<span className="dot">.</span><span className="dot">.</span><span className="dot">.</span></h1>}
